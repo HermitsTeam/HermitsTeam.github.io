ed1aad66f0b653da2ef483cdc899044a10bb33e9
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-12-29 16:53:17 -0300

    added Menu component and bootstrap import

--- portfolio_hermits/src/App.jsx
+++ portfolio_hermits/src/App.jsx
@@ -1,9 +1,22 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Menu from './components/Menu'
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="container">
+      <Router>
+        <Menu />
+        <Routes>
+          
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ portfolio_hermits/src/components/Menu.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+
+
+const Menu = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <ul className='nav navbar-nav me-auto'>
+                    <li className='nav-item'>
+                        
+                        <Link className='nav-link' to="/">Inicio</Link>
+                    </li>
+
+                    <li className='nav-item'>
+                        
+                        <Link className='nav-link' to="/">Contacto</Link>
+                    </li>
+                </ul>
+
+            </nav>
+        </div>
+    )
+}
+
+export default Menu
