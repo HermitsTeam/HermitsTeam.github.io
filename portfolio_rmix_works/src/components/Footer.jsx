46a52d69855934cc8cab453b5e4c4fb757fff27a
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2021-12-31 02:45:17 -0300

    Added Font Awesome

--- portfolio_rmix_works/src/components/Footer.jsx
+++ portfolio_rmix_works/src/components/Footer.jsx
@@ -1,12 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Footer = () => {
   return (
     <div>
-      <footer>
-        <div className="text-center p-4 bg-dark">
-         <p className="footer_text">© 2021 Copyright:</p>
+      <footer className="text-center p-4 bg-dark text-white">
+        <div>
+          <section class="mb-4">
+            <a
+              class="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-facebook-f"></i>
+            </a>
+
+            <a
+              class="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-twitter"></i>
+            </a>
+
+            <a
+              class="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-google"></i>
+            </a>
+
+            <a
+              class="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-instagram"></i>
+            </a>
+
+            <a
+              class="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+
+            <a
+              class="btn btn-outline-light btn-floating m-1"
+              href="#!"
+              role="button"
+            >
+              <i class="fab fa-github"></i>
+            </a>
+          </section>
+
+          <p className="footer_text">© 2021 Copyright:</p>
           <Link className="text-reset fw-bold" to="/">
             <p className="nav-link">RmiX Works</p>
           </Link>
